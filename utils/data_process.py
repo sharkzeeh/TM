3f687ea9f46396261cb5c5f6d10527db268e3285
Igor Babikov <igorbabikov24@gmail.com>
2020-05-24 15:58:53 +0300

    fix processing + delete pickle

--- utils/data_process.py
+++ utils/data_process.py
@@ -11,40 +11,29 @@ import os
 def single_call(fn):
     @functools.wraps(fn)
     def inner(*args, **kwargs):
-        if not inner.called or not os.path.exists('processed.pickle') or not os.stat(path).st_size:
-            fn(*args, **kwargs)
+        if not inner.called or not os.path.exists('processed.pickle') or not os.stat('processed.pickle').st_size:
+            res = fn(*args, **kwargs)
             inner.called = True
+            return res 
     inner.called = False
     return inner
 
 
 @single_call
-def process_text(text, min_word_size=2, min_sent_size=3, remove_short_sentenses=True):
-    html_cleaner = re.compile('<.*>')
-    cyrillic = re.compile(r'[^а-я ]')
+def process_text(text, min_word_size=4, min_sent_size=10, extra_stop=None, remove_short=True):
+    html_cleaner = re.compile('<.*?>')
+    cyrillic = re.compile(r'[^а-яА-Я ]')
     esc_punctuation = re.compile('[%s]' % re.escape(string.punctuation))
-    extra_spaces = re.compile(r'\s+')
-
-    stop = stopwords.words('russian')
+    extra_spaces = re.compile(r'\s{2,}')
+    stop_list = stopwords.words('russian')
+    if extra_stop:
+        stop_list.extend([''])
     lmtzr = Mystem()
     
     n_sents = len(text)
     for i in tqdm(range(n_sents)):
         # html removal
-
-        try:
-            text[i] = html_cleaner.sub(' ', text[i]) # there's NO NoneType error
-        except TypeError:
-            # text[i] = ''
-            print(f'deleting {text[i]} for some reason...')
-            del text[i]
-            continue
-
-        # make all words lowercased
-        # stopwords removal (Hint: look to nltk docs)
-        text[i] = ' '.join([word.lower() for word in text[i].split() \
-                            if not word in stop and len(word) > min_word_size])
-
+        text[i] = html_cleaner.sub(' ', text[i]) # there's NO NoneType error
         # punctuation and numbers removal
         text[i] = esc_punctuation.sub(' ', text[i])
         # leaving only cyrillic words
@@ -53,11 +42,10 @@ def process_text(text, min_word_size=2, min_sent_size=3, remove_short_sentenses=
         text[i] = extra_spaces.sub(' ', text[i])
         # lemmatization (Hint: look to pymystem3 docs)
         text[i] = ''.join(lmtzr.lemmatize(text[i])).strip()
-
-    if remove_short_sentenses:
-        print('removing short words...')
-        # remove texts with less than `min_sent_size` words after lemmatization   
-        text = [text[i] for text[i] in text if len(text[i].split()) > min_sent_size]
+        # stopwords removal + lowercasing
+        text[i] = ' '.join([word.lower() for word in text[i].split() \
+                            if len(word) >= min_word_size and word not in stop_list])
+        
+    text = [sent for sent in text if len(sent) >= min_sent_size]    
     print('done!')
-
     return text
